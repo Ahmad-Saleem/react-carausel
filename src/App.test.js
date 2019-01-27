5cf8718223d564784c88b3edd2be02b0c4dcb02e
Ahmad Saleem <it_saleem@hotmail.com>
2019-01-27 15:07:24 +0000

    add some render test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Slider from './components/Carousel/Slider';
+import { Arrow } from './components/Carousel/SVGIcons';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('render Slider', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Slider />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('render Arrow', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Arrow />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
